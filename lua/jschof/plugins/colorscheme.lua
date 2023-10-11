251b859bf8482fcbe97a5693d9d4ace37710fd34
Jim Schofield <23246869+JimSchofield@users.noreply.github.com>
2023-10-11 18:38:24 -0500

    Lint/formatting is working and integrated.  Term is fixed.  Finally picked nvim-tree and tokyo night

--- lua/jschof/plugins/colorscheme.lua
+++ lua/jschof/plugins/colorscheme.lua
@@ -1,7 +1,11 @@
 return {
-	"bluz71/vim-nightfly-guicolors",
-	priority = 1000,
+	"folke/tokyonight.nvim",
+	lazy = false,
 	config = function()
-		vim.cmd([[colorscheme nightfly]])
+		-- vim.cmd([[colorscheme tokyonight]])
+		vim.cmd([[colorscheme tokyonight-night]])
+		-- vim.cmd([[colorscheme tokyonight-storm]])
+		-- vim.cmd([[colorscheme tokyonight-day]])
+		-- vim.cmd([[colorscheme tokyonight-moon]])
 	end,
 }
